487c275a29c510657f44dca6099ea5708bd99af0
Joscelino Oliveira <joscelino.oliveira@gmail.com>
2021-02-14 19:06:19 -0300

    #4 Script de fila com prioridade

---
+++ pilhas_filas_deques/fila_prioridade.py
@@ -0,0 +1,57 @@
+import numpy as np
+
+
+class FilaPrioridade:
+    def __init__(self, capacidade):
+        self.capacidade = capacidade
+        self.numero_elementos = 0
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    def __fila_vazia(self):
+        return self.numero_elementos == 0
+
+    def __fila_cheia(self):
+        return self.numero_elementos == self.capacidade
+
+    def enfileirar(self, valor):
+        if self.__fila_cheia():
+            print('A fila esta cheia')
+            return
+
+        if self.numero_elementos == 0:
+            self.valores[self.numero_elementos] = valor
+            self.numero_elementos += 1
+        else:
+            x = self.numero_elementos - 1
+            while x >= 0:
+                if valor > self.valores[x]:
+                    self.valores[x + 1] = self.valores[x]
+                else:
+                    break
+                x -= 1
+            self.valores[x + 1] = valor
+            self.numero_elementos += 1
+
+    def desenfileirar(self):
+        if self.__fila_vazia():
+            print('A fila esta vazia!')
+            return
+
+        valor = self.valores[self.numero_elementos - 1]
+        self.numero_elementos -= 1
+        return valor
+
+    def primeiro(self):
+        if self.__fila_vazia():
+            return -1
+        return self.valores[self.numero_elementos - 1]
+
+
+fila = FilaPrioridade(5)
+print(fila.primeiro())
+fila.enfileirar(30)
+print(fila.primeiro())
+fila.enfileirar(50)
+print(fila.primeiro())
+fila.enfileirar(10)
+print(fila.primeiro())
